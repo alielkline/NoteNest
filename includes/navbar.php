a49a8e2f72c7e06a42323ed122226a1f300581a0
Ali Elkline <alielkline7@gmail.com>
2025-04-18 20:32:46 +0200

    Added baseline design

---
+++ includes/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-md">
+  <div class="container-fluid">
+    <!-- Logo + Name-->
+    <a class="navbar-brand" href="home.php"> 
+    <img src="../assets/logo.png" alt="NoteNest Logo">
+    NoteNest
+    </a>
+
+    <!-- Toggler button -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+      data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- The dropdown tabs -->
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+      <div class="navbar-nav">
+        <a class="nav-link" href="dashboard.php">Dashboard</a>
+        <a class="nav-link" href="classrooms.php">Classrooms</a>
+        <a class="nav-link" href="notes.php">Notes</a>
+        <a class="nav-link" href="contact.php">Contact</a>
+      </div>
+
+      <!-- log in and sign up buttons -->
+      <div class="auth-links">
+        <button class="btn-login">Log in</button>
+        <button class="btn-signup">Sign up</button>
+      </div>
+
+    </div>
+
+  </div>
+</nav>
