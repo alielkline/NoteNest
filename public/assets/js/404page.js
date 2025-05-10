78d1e1775b251799381ec86c3ccab8626e5f5a2f
Ali Elkline <alielkline7@gmail.com>
2025-05-10 16:04:57 +0300

    ContactPage + Some tweaks

---
+++ public/assets/js/404page.js
@@ -0,0 +1,9 @@
+function goHome() {
+    window.location.href = "/NoteNestMVC/";
+  }
+  
+  function reportProblem() {
+    alert("Reporting a problem. Please contact support or try again later.");
+    // You can replace this with an actual support link or form
+  }
+  
